7f887f5bbb570d4651dd5a4ec098ce97fe74e634
nkhoroshavin <74411205+nkhoroshavin@users.noreply.github.com>
2021-03-22 12:20:24 +0500

    Create 11.3.cpp

---
+++ 11.3.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+char a;
+
+struct stack
+{
+	char data;
+	stack* prev;
+};
+
+stack* stack_d(int n)
+{
+	if (n == 0)
+	{
+		return NULL;
+	}
+
+	stack* top, * p;
+	top = NULL;
+	p = new stack;
+
+	cout << "Введите элементы: ";
+	cin >> a;
+	p->data = a;
+	p->prev = NULL;
+	top = p;
+
+	for (int i = 2; i <= n; i++)
+	{
+		stack* h = new stack;
+
+		cin >> a;
+		h->data = a;
+		h->prev = top;
+		top = h;
+	}
+	return top;
+}
+
+void print(stack* top)
+{
+	if (top == NULL)
+	{
+		cout << "Стек пуст" << endl;
+	}
+
+	else
+	{
+		stack* p = top;
+
+		while (p != NULL)
+		{
+			cout << p->data << " ";
+			p = p->prev;
+		}
+		cout << endl;
+	}
+}
+
+void add(stack*& first, int n)
+{
+	stack* p = first;
+	stack* h = new stack;
+
+	cout << "Введите элемент для добавления: ";
+	cin >> h->data;
+	h->prev = p;
+	first = h;
+
+	while (p->prev != NULL)
+	{
+		stack* h = new stack;
+
+		cout << "Введите элемент для добавления: ";
+		cin >> h->data;
+		h->prev = p->prev;
+		p->prev = h;
+
+		if (p->prev->prev != NULL)
+		{
+			p = p->prev->prev;
+		}
+
+		else {
+			p = p->prev;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите кол-во элементов: ";
+	int n;
+	cin >> n;
+	stack* st = stack_d(n);
+	cout << "Стек: ";
+	print(st);
+	add(st, n);
+
+	cout << "Изменённый стек: ";
+	print(st);
+	return 0;
+}
