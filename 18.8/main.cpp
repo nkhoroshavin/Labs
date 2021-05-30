3e61edc1c8631ffae9ae41dcd0a99f73602518f1
nkhoroshavin <74411205+nkhoroshavin@users.noreply.github.com>
2021-05-30 23:46:07 +0500

    Add files via upload

---
+++ 18.8/main.cpp
@@ -0,0 +1,13 @@
+include <iostream>
+#include "person.h"
+#include "employee.h"
+#include "tree.h"
+#include "dialog.h"
+
+using namespace std;
+
+int main() {
+    dialog d;
+    d.execute();
+    return 0;
+}
