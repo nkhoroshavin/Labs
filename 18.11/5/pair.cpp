c5d1bf7a4381cb4d67dc868bc327db4a4c18af66
nkhoroshavin <74411205+nkhoroshavin@users.noreply.github.com>
2021-05-31 09:44:36 +0500

    Add files via upload

---
+++ 18.11/5/pair.cpp
@@ -0,0 +1,76 @@
+#include "Pair.h" 
+Pair::Pair()
+{
+	first = 0;
+	second = 0.0;
+}
+Pair::Pair(int f, double s)
+{
+	first = f;
+	second = s;
+}
+Pair::Pair(const Pair& t)
+{
+	first = t.first;
+	second = t.second;
+}
+Pair& Pair ::operator=(const Pair& t)
+{
+	if (&t == this) return *this;
+	first = t.first;
+	second = t.second;
+	return *this;
+}
+Pair Pair ::operator+(const Pair& t)
+{
+	first += t.first;
+	second += t.second;
+	return *this;
+}
+bool Pair:: operator<(const Pair& t) const
+{
+	if (first != t.first) {
+		return first < t.first;
+	}
+	else {
+		return second < t.second;
+	}
+}
+bool Pair:: operator>(const Pair& t) const
+{
+	if (first != t.first) {
+		return first > t.first;
+	}
+	else {
+		return second > t.second;
+	}
+}
+Pair Pair :: operator /(const int& t)
+{
+	first /= t;
+	second /= t;
+	return *this;
+}
+Pair Pair :: operator +=(const Pair& t)
+{
+	first += t.first;
+	second += t.second;
+	return *this;
+}
+bool Pair :: operator == (const Pair& t)
+{
+	return first == t.first && second == t.second;
+}
+ostream& operator << (ostream& out, const Pair& t)
+{
+	out << t.first << " : " << t.second;
+	return out;
+}
+istream& operator >> (istream& in, Pair& t)
+{
+	cout << "First?" << endl;
+	in >> t.first;
+	cout << "Second?" << endl;
+	in >> t.second;
+	return in;
+}
